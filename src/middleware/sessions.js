adc224f0595d040718aaa63f2d0c9e492f5f856c
Solene PC <solene.bary@gmail.com>
2020-05-01 14:37:42 +0100

    added titles + choose between 2 pseudos, takes the one smaller

--- src/middleware/sessions.js
+++ src/middleware/sessions.js
@@ -4,14 +4,37 @@ module.exports = (req, res, next) => {
         req.session.userID = 'userID' + makeid(9);
     }
 
+    const titles = [
+        'Dr.',
+        'Doc.',
+        'Prof.',
+        'Mr.',
+        'Miss',
+        'Mrs.',
+        'Sir',
+        'Lord',
+        'Lady'
+    ];
+
     if(!req.session.pseudo){
-        const randomName = uniqueNamesGenerator({ 
+        //try Adjective + Animal
+        const randomName1 = uniqueNamesGenerator({ 
             dictionaries: [adjectives, animals, countries],
             length: 2,
             separator: ' ',
             style: 'capital'
         });
-        req.session.pseudo = 'Dr. ' + randomName;
+        //Try Color + Animal
+        const randomName2 = uniqueNamesGenerator({ 
+            dictionaries: [colors, animals, countries],
+            length: 2,
+            separator: ' ',
+            style: 'capital'
+        });
+        //Take the one smaller
+        let name = [randomName1, randomName2].sort((a,b) => a.length - b.length)[0];
+        let title = chooseTitle(titles);
+        req.session.pseudo = title + ' ' + name;
     }
     next();
 }
@@ -24,4 +47,21 @@ makeid = (length) => {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
- }
+}
+
+chooseTitle = (array) => {
+    return shuffle(array)[0];
+}
+
+shuffle = (arr) => {
+    var i,
+        j,
+        temp;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;    
+};
