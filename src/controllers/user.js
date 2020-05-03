10ba3442e44289a79f351a4df9649f717a1569e3
Solene PC <solene.bary@gmail.com>
2020-05-03 13:03:34 +0100

    able to change the pseudo

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,3 +4,16 @@ exports.getUserId = (req,res,next) => {
         pseudo: req.session.pseudo
     });
 }
+
+exports.updatePseudo = (req,res,next) => {
+    const newP = req.body.pseudo;
+    if (newP.length < 20) {
+        req.session.pseudo = newP;
+        res.status(200).json({
+            pseudo: newP
+        })
+    } else {
+        return res.status(400).json({ error: 'Pseudo too long, should be under 20 characters'})
+    }
+}
+
