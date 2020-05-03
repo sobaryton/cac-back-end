386bc967594d5fe81a607286b34b10bd20a32219
Solene PC <solene.bary@gmail.com>
2020-05-03 14:05:46 +0100

    logout route

--- test/controller/user.get.test.js
+++ test/controller/user.get.test.js
@@ -14,5 +14,12 @@ describe('user session', () => {
       expect(res.body.userId).to.be.a('string');
       expect(res.body.pseudo).to.be.a('string');
     });
+    it('when I go to the site, there is a cookie with a user which is in the DB', async () => {
+      const res = await chai.request(app)
+      .get(`/user`);
+      expect(res.status).to.equal(200);
+      expect(res.headers['set-cookie'].length).to.equal(1);
+      expect(res.headers['set-cookie'][0].indexOf('CACoro')).to.be.above(-1);
+    });
   })
 });
